8781b119a3072088529a4aaec781572863ecf048
Adolfans <a.dolfans@gmail.com>
2012-04-25 21:19:02 +0800

    binary files

---
+++ bin/config.lua
@@ -0,0 +1,12 @@
+width = 800
+height = 600
+tittle = "My World"
+gversion = 9
+print(tittle)
+print("config.lua loaded")
+a = 1
+if a ==1  then
+tittle = "My world - a ==1!!"
+else
+tittle = "My world - a !=1"
+end
